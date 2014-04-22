630411bfcf6b98b953d0ecf8a7e1210d08a2e126
bbraithwaite <bradley.braithwaite@gmail.com>
2014-04-23 00:00:05 +0200

    initial test coverage and refactoring

--- build/index.js
+++ build/index.js
@@ -1,18 +1,5 @@
 var install = require("./install");
-
-/*
+var path = require("path");
 var projectPath = path.resolve(__dirname, "../../../");
-var filePath = path.join(__dirname, "files");
-var packagePath = path.join(projectPath, "package.json");
-var projectName = path.basename(projectPath);
-var gitPath = path.join(projectPath, ".git");
-var pcPath = path.join(gitPath, "hooks", "pre-commit");
-var postCheckoutPath = path.join(gitPath, "hooks", "post-checkout");
-var jsiPath = path.join(projectPath, ".jshintignore");
-var jsrcPath = path.join(projectPath, ".jshintrc");
-var pcModulePath = path.join(projectPath, "../", ".git", "modules", projectName, "hooks");
-*/
 
-module.exports = function () {
-  install.init(__dirname, projectPath);
-};
+install.init(__dirname, projectPath);
