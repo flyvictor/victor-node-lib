418665e352887ba65552a72732bd840574439929
KKudryavtsev <konstantin.k.ed@gmail.com>
2014-05-12 17:56:55 +0100

    added request-promise

---
+++ request-promise/index.js
@@ -0,0 +1,32 @@
+var request = require("request"),
+    when = require("when"),
+    _ = require("underscore");
+
+//wraps request's http methods as promises, preserving function signatures and all other methods 
+module.exports = _.reduce(["get", "post", "put", "del", "patch"], function(memo, methodName){
+  memo[methodName] = function(){
+    var deferred = when.defer();
+
+    if (arguments[0].debug) {
+      var url = arguments[0].url;
+
+      if (arguments[0].qs) {
+        url += "?" + _.reduce(arguments[0].qs, function(memo, value, key) {
+          return (memo ? memo + "&" : "") + key + "=" + value;
+        }, null);
+      }
+
+      console.log("request-promise making %s request to %s", methodName, url);
+    }
+
+    request[methodName].apply(request, _.union(arguments, function(err, data){
+      return err ? deferred.reject(err) : deferred.resolve(data);
+    }));
+
+    return deferred.promise;
+  };
+  
+  return memo;
+}, _.clone(request));
+
+
