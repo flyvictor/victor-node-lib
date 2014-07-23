3b2c00f65f4f7659117473bd6b175d0fdb370876
Rockafella <eugene.kostrikov@gmail.com>
2014-07-23 18:56:41 +0400

    Apply credentials parser as fortune hook

--- access-policy/index.js
+++ access-policy/index.js
@@ -1,16 +1,34 @@
 var _ = require("underscore");
+var when = require("when");
 
 var policies = require("./policies");
 
+var _credentialsParser = require("../filters/credentialsParser");
+var credentialsParserHook = {
+  name: "credentialsParser",
+  init: function(){
+    return function(req, res){
+      var d = when.defer();
+      var self = this;
+      _credentialsParser(req, res, function(){
+        d.resolve(self);
+      });
+      return d.promise;
+    }
+  }
+};
+
 module.exports = {
-  authHooks: [
-    _.extend(require("./augment-user-hook").hook, {priority: 100}),
-    _.extend(require("./enforce-valid-token-hook").hook, {priority: 90}),
-    _.extend(require("./set-access-policies"), {priority: 80}),
-    _.extend(require("./restrict-resource-access"), {priority: 70})
-  ],
   policies: policies,
   setPolicies: function(policies){
     this.policies = policies;
-  }
+  },
+  authHooks: [
+    _.extend(credentialsParserHook, {priority: 100}),
+    _.extend(require("./augment-user-hook").hook, {priority: 90}),
+    _.extend(require("./set-access-policies"), {priority: 80}),
+    _.extend(require("./restrict-resource-access"), {priority: 70})
+  ],
+  augmentUser: require("./augment-user-hook").augmentUser,
+  enforceValidToken: require("./enforce-valid-token-hook").enforceValidToken
 };
