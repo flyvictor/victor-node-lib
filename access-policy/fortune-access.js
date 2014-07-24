06089603fd8d001150f42adc32a7013215e4ba4c
Rockafella <eugene.kostrikov@gmail.com>
2014-07-24 15:14:40 +0400

    Extend fortune-access

--- access-policy/fortune-access.js
+++ access-policy/fortune-access.js
@@ -4,16 +4,16 @@ var restrictResourceAccess = require("./restrict-resource-access");
 
 
 function FortuneAccess(policies, adminRoles, adminApps){
+  _.each(policies, this._validatePolicy);
   this._policies = policies;
-  this._adminRoles = adminRoles;
-  this._adminApps = adminApps;
+  this._adminRoles = adminRoles || [];
+  this._adminApps = adminApps || [];
   this._middleware = [];
-  this._hooks = [];
   this._priority = 100;
 }
 
 FortuneAccess.prototype.init = function(apps){
-  _.each(apps, this._applyPolicy);
+  _.each(apps, this._setupHooks);
 };
 
 FortuneAccess.prototype.useC = function(fn){
@@ -36,47 +36,46 @@ FortuneAccess.prototype.setupHttpFilters = function(express){
   return this;
 };
 
-/**
- * Consumes function that will extend request in some way
- * Should require no additional config
- */
-FortuneAccess.prototype.setReqAugmentors = function(augmentors){
-  var self = this;
-  this._hooks.concat(_.map(augmentors, function(constructor){
-    _.extend(constructor, {priority: self._priority--});
-  }));
+FortuneAccess.prototype._validatePolicy = function(policy){
+  var valid = true;
 
-  return this;
+  if(!existy(policy, "name") || !existy(policy, "predicate")) valid = false;
+  if(!_.isFunction(policy.predicate)) valid = false;
+
+  if (!valid) throw new Error("Policy " + policy.name +" is invalid");
+
+  function existy(obj, prop){
+    return !_.isUndefined(obj[prop]) && !_.isNull(obj[prop]);
+  }
 };
 
-FortuneAccess.prototype._applyPolicy = function(app){
-  //Read each resource policy
-  var policies = app.policy;
-  //Attach validation hooks
-  this._setupHooks(app, policies);
-};
-
-FortuneAccess.prototype._setupHooks = function(app, policies){
+FortuneAccess.prototype._setupHooks = function(app){
   var self = this;
 
   //TODO: extend fortune to provide public .eachResource method
   _.each(app._resources, function(resource, name){
-    var predicates = self._getPredicates(policies);
-    app.beforeRW(name, [_.extend(setAccessPolicies, {priority: self._priority--})], {
-      predicates: predicates,
-      adminRoles: self._adminRoles,
-      adminApps: self._adminApps
-    });
-
-    app.beforeRW(name, [_.extend(restrictResourceAccess, {priority: self._priority--})], {
-      policies: app.policy
+    var predicates = self._getPredicates(resource);
+    app.beforeRW(name, [
+        _.extend(setAccessPolicies, {priority: self._priority--}),
+        _.extend(restrictResourceAccess, {priority: self._priority--})
+      ], {
+       "set-access-policies": {
+          predicates: predicates,
+          adminRoles: self._adminRoles,
+          adminApps: self._adminApps
+       },
+       "restrict-resource-access": {
+         policies: resource.policy
+       }
     });
   });
 };
 
 FortuneAccess.prototype._getPredicates = function(resource){
   var policies = Object.keys(resource.policy || {});
-  return _.pick.apply(null, [this._policies].concat(policies));
+  return _.filter(this._policies, function(p){
+    return policies.indexOf(p.name) !== -1;
+  });
 };
 
 
--- test/access-policy/fortune-access.js
+++ test/access-policy/fortune-access.js
@@ -1,3 +1,4 @@
+var _ = require("lodash");
 var FortuneAccess = require("../../access-policy/fortune-access");
 
 describe("FortuneAccess", function(){
@@ -31,4 +32,64 @@ describe("FortuneAccess", function(){
     express.mw.length.should.equal(2);
   });
 
+  it("should be able to consume augmentor hooks and define highest priority on them", function(){
+
+  });
+
+  it("should throw if provided policy is invalid", function(){
+    (function(){
+      FortuneAccess([{name: null}]);
+    }).should.throw("Policy null is invalid");
+  });
+
+  it("should be able to get proper policy predicate functions", function(){
+    var pOne = {name: "one", predicate: function(){}};
+    var pTwo = {name: "two", predicate: function(){}};
+    var itAccess = FortuneAccess([pOne, pTwo]);
+    var preds = itAccess._getPredicates({policy: {one: {}}});
+    preds.length.should.equal(1);
+    preds[0].name.should.equal("one");
+  });
+
+  it("should not fail in resource has no policy set up", function(){
+    (function(){
+      var itAccess = FortuneAccess({
+        one: {name: "one", predicate: function(){}},
+        two: {name: "two", predicate: function(){}}
+      });
+      var preds = itAccess._getPredicates({});
+      preds.should.eql([]);
+    }).should.not.throw();
+  });
+
+  it("should be able to set configured hooks on provided resources", function(){
+    var app = {
+      hooks: {},
+      beforeRW: function(name, array, config){
+        this.hooks[name] = this.hooks[name] || {};
+        array.forEach(function(h){
+          var clone = _.cloneDeep(h);
+          clone.config = config[clone.name];
+          this.hooks[name][clone.name] = clone;
+        }, this);
+      },
+      _resources: {
+        one: {policy: {firstPolicy: {}}},
+        two: {policy: {}},
+        three: {}
+      }
+    };
+    var fa = FortuneAccess([
+      {name: 'firstPolicy', predicate: function(){}},
+      {name: 'secondPolicy', predicate: function(){}}
+    ], ["adminRole"], ["adminApp"]);
+    fa._setupHooks(app);
+    var oneSet = app.hooks.one["set-access-policies"];
+    oneSet.config.predicates.length.should.equal(1);
+    oneSet.config.adminRoles.length.should.equal(1);
+    var oneRestrict = app.hooks.one["restrict-resource-access"];
+    oneRestrict.config.policies.firstPolicy.should.be.Object;
+
+    oneSet.priority.should.be.greaterThan(oneRestrict.priority);
+  });
 });
